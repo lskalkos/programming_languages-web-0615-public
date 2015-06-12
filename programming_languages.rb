61f26c78afa049c54371eb28174d5bdc0bd97963
Lia Skalkos <hello@liaskalkos.net>
2015-06-12 13:43:50 -0400

    2 refactoring

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,54 +1,16 @@
-require 'pry'
-
-# input = {
-#   :oo => {
-#     :ruby => {
-#       :type => "interpreted"
-#     },
-#     :javascript => {
-#       :type => "interpreted"
-#     },
-#     :python => {
-#       :type => "interpreted"
-#     },
-#     :java => {
-#       :type => "compiled"
-#     }
-#   },
-#   :functional => {
-#     :clojure => {
-#       :type => "compiled"
-#     },
-#     :erlang => {
-#       :type => "compiled"
-#     },
-#     :scala => {
-#       :type => "compiled"
-#     },
-#     :javascript => {
-#       :type => "interpreted"
-#     }
-
-#   }
-# }
 
 def reformat_languages(language_styles)
   # your code here
 
-  hash_by_languages = {}
 
-
-  language_styles.each do |style, languages|
+  language_styles.each_with_object({}) do |(style, languages), hash_by_languages|
     languages.each do |language, type|
 
       if hash_by_languages[language]
         hash_by_languages[language][:type] = type.values.first
         hash_by_languages[language][:style] << style
       else
-        hash_by_languages[language] = {
-          type: "",
-          style: []
-        }
+        hash_by_languages[language] = Hash.new([])
         hash_by_languages[language][:type] = type.values.first
         hash_by_languages[language][:style] << style
       end
@@ -56,11 +18,8 @@ def reformat_languages(language_styles)
     end
   end
 
-  hash_by_languages
 
 end
-
-# reformat_languages(input)
 
 
 
