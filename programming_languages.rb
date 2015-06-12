38f362e269ca2011df79aa4a7662a87c8dd9dd86
Lia Skalkos <hello@liaskalkos.net>
2015-06-12 13:29:35 -0400

    Complete reformat_languages method

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,67 @@
+# require 'pry'
+
+# input = {
+#   :oo => {
+#     :ruby => {
+#       :type => "interpreted"
+#     },
+#     :javascript => {
+#       :type => "interpreted"
+#     },
+#     :python => {
+#       :type => "interpreted"
+#     },
+#     :java => {
+#       :type => "compiled"
+#     }
+#   },
+#   :functional => {
+#     :clojure => {
+#       :type => "compiled"
+#     },
+#     :erlang => {
+#       :type => "compiled"
+#     },
+#     :scala => {
+#       :type => "compiled"
+#     },
+#     :javascript => {
+#       :type => "interpreted"
+#     }
+
+#   }
+# }
+
 def reformat_languages(languages)
   # your code here
+
+  hash_by_languages = {}
+
+  languages.each do |style, languages|
+    languages.keys.each do |language|
+      hash_by_languages[language] = {
+        type: "",
+        style: []
+      }
+    end
+  end
+
+  languages.each do |style, languages|
+    languages.each do |language, type|
+      hash_by_languages[language][:type] = type.values.first
+      hash_by_languages[language][:style] << style
+    end
+  end
+
+  hash_by_languages
+
 end
+
+# reformat_languages(input)
+
+
+
+
+
+
+
